0066d2f996fc170471f5c3768611bab3a9c35401
Adelle Pitsas <apitsas4@gmail.com>
2022-08-29 19:02:29 -0400

    complete unicorn tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,7 +2,22 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+  if (color === undefined) {
+    this.color = 'white';
+  } else {
+    this.color = color; }
+  }
+  isWhite() {
+    console.log(this.color)
+    if (this.color !== 'white') {
+      return false
+    }
+  }
+  says(message) {
+    return `**;* ${message} *;**`
   }
 }
+
+
 
 module.exports = Unicorn;
