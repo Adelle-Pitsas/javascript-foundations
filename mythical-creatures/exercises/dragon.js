200d39432e826bd6b24bc0d700622307b102bd7c
Adelle Pitsas <apitsas4@gmail.com>
2022-08-30 09:41:31 -0400

    created dragon with name, rider, hungry and greeting

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,17 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name
+    this.rider = rider
+    this.hungry = true
+  }
+  greet(greeting) {
+      return `Hi, ${this.rider}!`
+  }
+  eat() {
+    var baseEat = 0
+    var timesEaten = 
+    }
+}
 
+
+module.exports = Dragon
