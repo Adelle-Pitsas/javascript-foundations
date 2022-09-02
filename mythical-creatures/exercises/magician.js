c66ef5e663e48fce309eeb1f38863e2cd002f8de
Adelle Pitsas <apitsas4@gmail.com>
2022-09-02 08:59:11 -0400

    complete tests through gain confidence

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,11 +1,30 @@
 class Magician {
   constructor(magician) {
-    this.name = `The Great ${magician.name}`
-    this.assistant = magician.assistant
+    this.name = `The Great ${magician.name}`;
+    this.assistant = magician.assistant;
+    if (magician.clothing === undefined) {
+      this.favoriteAccessory = "top hat"
+    } else {
+      this.favoriteAccessory = magician.clothing
+    }
+    this.confidencePercentage = 10
+  }
+
+  performIncantation(message) {
+    var incantation = message.toUpperCase()
+    return (`${incantation}!`)
+  }
+
+  performTrick() {
+    this.confidencePercentage+=10
+    if (this.favoriteAccessory === "top hat") {
+      return "PULL RABBIT FROM TOP HAT"
+    } else {
+      return "PULL DOVE FROM SLEEVE"
+    }
   }
 }
 
-//the argument being passed through on lines 19 and 20 are objects in themselves
-//So I need to find a way to access that whole object
+
 
 module.exports = Magician
