936f25a5a8ce47a89c19efa787f14dbf2761a846
Adelle Pitsas <apitsas4@gmail.com>
2022-09-06 08:47:54 -0400

    complete all magician tests

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -23,6 +23,14 @@ class Magician {
       return "PULL DOVE FROM SLEEVE"
     }
   }
+
+  performShowStopper() {
+    if(this.confidencePercentage >= 100 && this.assistant) {
+      return "WOW! The magician totally just sawed that person in half!"
+    } else {
+      return "Oh no, this trick is not ready!"
+    }
+  }
 }
 
 
